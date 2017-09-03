99d5b76fc59bdee7ff3ee48838d18eb9962f54c4
Stepan Pilshchikov <pilshchikov.s.n@gmail.com>
2017-09-03 21:41:14 +0500

    README build scripts tweaks

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ ng build --output-path ./../server/src/main/resources/static/ --prod -env=full
 cd ../server/
 mvn clean package
 cd ../
-docker build -t your_local_docker_registry/utils.web:cutted --build-arg VERSION=FULL .
-docker push your_local_docker_registry/utils.web:full
+docker build -t utils.web:cutted --build-arg VERSION=FULL .
 docker rmi $(docker images | grep 'utils.web' | grep 'none' | awk '{print $3}')
 rm -rf server/src/main/resources/static/
